fc0d0de892def50a1176549bff212d8e09059c88
clara <10088152+claraj@users.noreply.github.com>
2021-02-10 14:50:21 -0600

    extra message

--- recursion_error.py
+++ recursion_error.py
@@ -1,7 +1,7 @@
 def main():
     print('this is the main function!')
     main()   # main calls itself - this is known as recursion
-    print('after the call to main')
+    print('this never prints because main never returns')
 
 if __name__ == '__main__':
     main()
